fa7be63c86ac24b9dddd54deb5032e51576333b4
Farelito <bakoufarel@gmail.com>
2024-06-06 12:18:37 +0200

    new class place

--- models/place.py
+++ models/place.py
@@ -2,31 +2,25 @@
 """
 This module contains classes representing model base.
 """
+# models/place.py
 from datetime import datetime
-from models.base_model import BaseModel
 
-
-class Place(BaseModel):
-    def __init__(self, name, description, address, city_id, latitude,
-                 longitude, host_id, num_rooms, num_bathrooms, price_per_night,
-                 max_guests, amenities):
-        super().__init__()
+class Place:
+    def __init__(self, id, name, description, price, user_id, city_id):
+        self.id = id
         self.name = name
         self.description = description
-        self.address = address
+        self.price = price
+        self.user_id = user_id
         self.city_id = city_id
-        self.latitude = latitude
-        self.longitude = longitude
-        self.host_id = host_id
-        self.num_rooms = num_rooms
-        self.num_bathrooms = num_bathrooms
-        self.price_per_night = price_per_night
-        self.max_guests = max_guests
-        self.amenities = amenities
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        self.amenities = []
+        self.reviews = []
 
     def add_amenity(self, amenity):
         if amenity not in self.amenities:
-            self.amenities.add(amenity)
+            self.amenities.append(amenity)
             self.updated_at = datetime.now()
 
     def get_reviews(self):
