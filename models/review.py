11452f902883fd1f6bd563e45b9e58aa06031bab
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-06-06 17:09:53 +0200

    review class in models

---
+++ models/review.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+class Review:
+    def __init__(self, review_id, user_id, place_id, rating, comment):
+        self.review_id = review_id
+        self.user_id = user_id
+        self.place_id = place_id
+        self.rating = rating
+        self.comment = comment
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
