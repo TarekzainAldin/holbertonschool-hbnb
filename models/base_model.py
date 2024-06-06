b2973f44401a95a0ddf33b2747c195e7b0cbf309
Jlaborne <laborne.j@hotmail.fr>
2024-06-06 13:44:02 +0200

    Added user.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@ import uuid
 
 from datetime import datetime
 
-class BasicModel:
+class BaseModel:
     def __init__(self) :
         self.id=str(uuid.uuid4())
         self.created_at = datetime.now()
