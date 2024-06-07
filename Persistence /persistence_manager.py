7219d5c82c9186cd81928914744566a16fb5c272
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-06-07 15:52:32 +0200

    task2. Persistence Implementation make the interface and make the datamanger class and make the test by tarek

---
+++ Persistence /persistence_manager.py
@@ -0,0 +1,18 @@
+# hbnb_evolution/persistence/persistence_manager.py
+
+from abc import ABC, abstractmethod
+
+class IPersistenceManager(ABC);
+    @abstractmethod
+    def save(self,entity):
+        pass
+
+    @abstractmethod
+    def get(self, entity_id, entity_type):
+        pass
+    @abstractmethod
+    def update(self, entity):
+        pass
+    @abstractmethod
+    def delete(self, entity_id,entyty_type):
+        pass 
