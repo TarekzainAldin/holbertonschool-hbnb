e714c9a4d5b199712979a17ae369550a6eb49121
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-06-08 13:05:18 +0200

    fix the error and edit the test_amenitites

--- tests/test_amenity.py
+++ tests/test_amenity.py
@@ -1,11 +1,16 @@
+# tests/test_amenity.py
 import unittest
+from datetime import datetime
 from models.amenitites import Amenity
 
 class TestAmenity(unittest.TestCase):
     def test_amenity_creation(self):
-        amenity = Amenity(name="Pool")
-        self.assertIsInstance(amenity.id, str)
-        self.assertEqual(amenity.name, "Pool")
+        amenity = Amenity("Wi-Fi")
+        self.assertEqual(amenity.name, "Wi-Fi")
+        self.assertIsInstance(amenity.created_at, datetime)
+        self.assertIsInstance(amenity.updated_at, datetime)
 
-if __name__ == "__main__":
+    # Add more tests for other methods and business logic
+
+if __name__ == '__main__':
     unittest.main()
