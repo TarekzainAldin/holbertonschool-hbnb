7d3d997815c732d577c3d7b3c44f5129a9dab414
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-06-08 13:00:52 +0200

    edite the amenity

--- models/amenitites.py
+++ models/amenitites.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
-"""Define Amenities class"""
-from base_model import BaseModel
-
-
-class Amenities(BaseModel):
+# model/amenity.py
+class Amenity:
     def __init__(self, name):
-        super().__init__()
         self.name = name
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __repr__(self):
+        return f"Amenity(name='{self.name}')"
